874260180ec4afa73fa5baa53c9612fa83d403d2
yana <1032216233@pfur.ru>
2024-05-28 16:32:12 +0300

    add to-do

--- jscounter/index.js
+++ jscounter/index.js
@@ -12,3 +12,6 @@ function decrement() {
     document.getElementById("counter_text").textContent = counter;
 }
 //function documentReady(){присвоить increment к html}
+//создать многостраничный сайт с вводом в форму
+//картинки? js? создать to-do list на странице
+//header в одном файле, добавлять на каждую страницу через js
