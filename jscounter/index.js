0ba51d931469770bfd314a30ed6587c7c2dbb4a8
yana <1032216233@pfur.ru>
2024-05-28 16:09:51 +0300

    initalizing, pull to vscode

---
+++ jscounter/index.js
@@ -0,0 +1,8 @@
+let counter=0;
+
+function increment(){
+    counter++;
+    console.log(counter);
+    document.getElementById("counter_text").textContent=counter;
+}
+//function documentReady(){присвоить increment к html}
