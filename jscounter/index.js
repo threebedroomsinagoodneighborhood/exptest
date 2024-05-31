16e3e70381f149ac32d0d2f76582de26ea281f0e
yana <1032216233@pfur.ru>
2024-05-31 22:46:48 +0300

    add comments

--- jscounter/index.js
+++ jscounter/index.js
@@ -1,3 +1,5 @@
+//первые кнопки для изменения цифры
+
 let counter = 0;
 
 function increment() {
@@ -11,7 +13,28 @@ function decrement() {
     console.log(counter);
     document.getElementById("counter_text").textContent = counter;
 }
-//function documentReady(){присвоить increment к html}
+
+//будущие функции
+
+function changesize() {
+    //изменяет шрифт всей страницы по кнопкам incr и decr
+    //<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">incr</button>
+}
+
+function addtolist() {
+    //добавляет новые строки к выбору по галочкам
+}
+
+//дата и время
+
+function getdate() {
+    console.log(Date());
+    document.getElementById("showdate").innerHTML = new Date();
+}
+
+//function documentReady(){}//присвоить increment к html?
 //создать многостраничный сайт с вводом в форму
 //картинки? js? создать to-do list на странице
 //header в одном файле, добавлять на каждую страницу через js
+
+//моя работа заключается в верстке сайта инженерки поэтому мне ориентироваться надо на ...
