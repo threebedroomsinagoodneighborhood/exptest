e64876ca63b96d4a6386766e46c0a09bc5ff49a4
yana <1032216233@pfur.ru>
2024-06-03 23:50:12 +0300

    added second page

--- jscounter/index.js
+++ jscounter/index.js
@@ -1,3 +1,4 @@
+//стр index
 //первые кнопки для изменения цифры
 
 let counter = 0;
@@ -14,17 +15,6 @@ function decrement() {
     document.getElementById("counter_text").textContent = counter;
 }
 
-//будущие функции
-
-function changesize() {
-    //изменяет шрифт всей страницы по кнопкам incr и decr
-    //<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">incr</button>
-}
-
-function addtolist() {
-    //добавляет новые строки к выбору по галочкам
-}
-
 //дата и время
 
 function getdate() {
@@ -32,9 +22,22 @@ function getdate() {
     document.getElementById("showdate").innerHTML = new Date();
 }
 
+//будущие функции
+
+function changesize() {
+    //изменяет шрифт всей страницы по кнопкам incr и decr
+    //<button type="button" onclick="document.getElementById('*').style.fontSize='35px'">incr</button>
+}
+
+function addtolist() {
+    //добавляет новые строки к выбору по галочкам
+}
+
 //function documentReady(){}//присвоить increment к html?
 //создать многостраничный сайт с вводом в форму
 //картинки? js? создать to-do list на странице
-//header в одном файле, добавлять на каждую страницу через js
 
-//моя работа заключается в верстке сайта инженерки поэтому мне ориентироваться надо на ...
+//header в одном файле, добавлять на каждую страницу через js
+//stackoverflow дал это но оно вроде для копии хедера с онлайн сайтов?
+//const resp = await fetch(document.location.href, {method: 'HEAD'});
+//const headers = Object.fromEntries(resp.headers.entries());
